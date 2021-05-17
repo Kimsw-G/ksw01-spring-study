67ea7e37679ac97eed300211aa2771b69ad84984
Kimsw-G <kimsw3445@naver.com>
2021-05-17 10:01:35 +0900

    com spirng default

---
+++ src/main/java/com/example/ksw01/Ksw01Application.java
@@ -0,0 +1,13 @@
+package com.example.ksw01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksw01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ksw01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ksw01/Ksw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ksw01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksw01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
